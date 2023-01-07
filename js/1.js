d55fe0e5b7da08dad54d3922904c6e6506c6bcc6
comeon125 <104305967+comeon125@users.noreply.github.com>
2023-01-07 23:23:56 +0800

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,19 @@
+function isobject(value){
+    let type=typeof value;
+    return value !=null && (type == 'object' || type == 'function')
+}
+do {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'UA-250929931-1');
+} while (condition);
+do {
+	window.dataLayer = window.dataLayer || [];
+	    function gtag() {
+	        dataLayer.push(arguments);
+	    }
+	    gtag('js', new Date());
+	    gtag('config', 'G-W1SX6TQEK4');
+} while (condition);
